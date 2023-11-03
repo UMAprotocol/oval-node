94c28d375fd627dea0b2f0fe45aa5c33c6a82893
Matt Rice <matthewcrice32@gmail.com>
2023-11-03 09:33:58 -0400

    fix: clean up types and functions for env parsing
    Signed-off-by: Matt Rice <matthewcrice32@gmail.com>

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,20 +2,18 @@ import { getAddress } from "ethers";
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
-const environmentError = (varName: string) => new Error(`Environment error: ${varName} not set.`);
-
-if (!process.env.AUTH_PRIVATE_KEY) throw environmentError("AUTH_PRIVATE_KEY");
-if (!process.env.SENDER_PRIVATE_KEY) throw environmentError("SENDER_PRIVATE_KEY");
-if (!process.env.PROVIDER_URL) throw environmentError("PROVIDER_URL");
-if (!process.env.OEV_ORACLE_ADDRESS) throw environmentError("OEV_ORACLE_ADDRESS");
-if (!process.env.FORWARD_URL) throw environmentError("FORWARD_URL");
+function getEnvVar(varName: string, defaultValue?: string): string {
+  const envValue = process.env[varName];
+  if (envValue !== undefined) return envValue;
+  if (defaultValue !== undefined) return defaultValue;
+  throw new Error(`Environment error: ${varName} not set.`);
+}
 
 export const env = {
-  authKey: process.env.AUTH_PRIVATE_KEY || "",
-  providerUrl: process.env.PROVIDER_URL || "",
-  providerWss: process.env.PROVIDER_WSS || "",
-  oevShareBuilder: process.env.OEV_SHARE_BUILDER || "",
-  senderKey: process.env.SENDER_PRIVATE_KEY || "",
-  forwardUrl: process.env.FORWARD_URL || "https://relay.flashbots.net",
-  oevOracle: getAddress(process.env.OEV_ORACLE_ADDRESS) || "0",
+  authKey: getEnvVar("AUTH_PRIVATE_KEY"),
+  providerUrl: getEnvVar("PROVIDER_URL"),
+  providerWss: getEnvVar("PROVIDER_WSS"),
+  senderKey: getEnvVar("SENDER_PRIVATE_KEY"),
+  forwardUrl: getEnvVar("FORWARD_URL"),
+  oevOracle: getAddress(getEnvVar("OEV_ORACLE_ADDRESS")),
 };
