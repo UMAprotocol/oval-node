a2e60f1d1d34f979228b2c8324d4ab9f00de66fb
Reinis Martinsons <reinis@umaproject.org>
2023-11-08 10:29:07 +0000

    fix: remove unused wss provider
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -14,7 +14,6 @@ function getEnvVar(varName: string, defaultValue?: string): string {
 export const env = {
   authKey: getEnvVar("AUTH_PRIVATE_KEY"),
   providerUrl: getEnvVar("PROVIDER_URL"),
-  providerWss: getEnvVar("PROVIDER_WSS"),
   senderKey: getEnvVar("SENDER_PRIVATE_KEY"),
   forwardUrl: getEnvVar("FORWARD_URL", fallback.forwardUrl),
   oevOracle: getAddress(getEnvVar("OEV_ORACLE_ADDRESS", fallback.oevOracle)),
