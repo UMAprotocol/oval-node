ce290258da09f6cd1a4810a3808ac09f3d816867
Pablo Maldonado <pablo@umaproject.org>
2023-11-23 15:26:18 +0100

    feat: clean
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- src/lib/logging.ts
+++ src/lib/logging.ts
@@ -32,18 +32,3 @@ export class Logger {
     };
   }
 }
-
-export function createShortHexString(hex: string): string {
-  return hex.substring(0, 5) + "..." + hex.substring(hex.length - 6, hex.length);
-}
-
-export function createEtherscanLinkMarkdown(hex: string): string | null {
-  const url = "https://etherscan.io/";
-  if (hex.substring(0, 2) != "0x") return null;
-  const shortURLString = createShortHexString(hex);
-  // Transaction hash
-  if (hex.length == 66) return `<${url}tx/${hex}|${shortURLString}>`;
-  // Account
-  else if (hex.length == 42) return `<${url}address/${hex}|${shortURLString}>`;
-  return null;
-}
